ea163aac717ed02c54795825e46edc84b971011b
Dmitry Afonchenko <undermove@yandex.ru>
2020-10-04 12:49:05 +0300

    add first test

---
+++ src/MathCalculatorTests/MathCalculatorTests/MathCalculatorTest.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace MathCalculatorTests
+{
+    public class MathCalculatorTest
+    {
+        [Fact]
+        public void WhenSumFiveAndThreeThenGotEight()
+        {
+            // Arrange
+            const int Five = 5;
+            const int Three = 3;
+            MathCalculator calculator = new MathCalculator();
+        }
+    }
+}
