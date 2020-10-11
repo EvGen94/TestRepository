ede3138674163c56880a18cf29891ccc9a41b53f
Dmitry Afonchenko <undermove@yandex.ru>
2020-10-11 12:28:23 +0300

    remove redundant changes

--- src/MathCalculatorTests/MathCalculatorTests/MathCalculator.cs
+++ src/MathCalculatorTests/MathCalculatorTests/MathCalculator.cs
@@ -6,11 +6,6 @@ namespace MathCalculatorTests
         public int Sum(int a, int b){
             return a+b;
         }
-        
-        public int Sum(string a, string b)
-        {
-            return Convert.ToInt32(a) + Convert.ToInt32(b);
-        }
 
         public int Sub(int a, int b)
         {
