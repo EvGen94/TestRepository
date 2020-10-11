d4ec8853629fb2be62cc97b605f1adaf94cf32b0
fox <ivan51673@gmail.com>
2020-10-11 12:05:49 +0300

    int Sum(string a, string b)

--- src/MathCalculatorTests/MathCalculatorTests/MathCalculator.cs
+++ src/MathCalculatorTests/MathCalculatorTests/MathCalculator.cs
@@ -1,9 +1,14 @@
-namespace MathCalculatorTests 
+namespace MathCalculatorTests
 {
     public class MathCalculator
     {
-        public int Sum(int a, int b){
-            return a+b;
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sum(string a, string b)
+        {
+            return int.Parse(a) + int.Parse(b);
         }
 
         public int Sub(int a, int b)
