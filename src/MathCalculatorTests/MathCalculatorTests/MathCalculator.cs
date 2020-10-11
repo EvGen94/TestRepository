369a069696234aa71d48213f3d14a89b2ec4d8ee
User <example@example.com>
2020-10-11 12:06:06 +0300

    Added Sum function for two string parameters

--- src/MathCalculatorTests/MathCalculatorTests/MathCalculator.cs
+++ src/MathCalculatorTests/MathCalculatorTests/MathCalculator.cs
@@ -2,13 +2,21 @@ namespace MathCalculatorTests
 {
     public class MathCalculator
     {
-        public int Sum(int a, int b){
-            return a+b;
+        public int Sum(int a, int b)
+        {
+            return a + b;
         }
 
         public int Sub(int a, int b)
         {
             return a - b;
+        }
+
+        public int Sum(string a, string b)
+        {
+            int p = int.Parse(a);
+            int q = int.Parse(b);
+            return p + q;
         }
     }
 }
