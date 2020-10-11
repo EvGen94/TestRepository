9737d9ab1a5f00b5f3f009564adc6ebd490affd1
realplaya1 <39435205+realplaya1@users.noreply.github.com>
2020-10-11 12:06:17 +0300

    Add overload

--- src/MathCalculatorTests/MathCalculatorTests/MathCalculator.cs
+++ src/MathCalculatorTests/MathCalculatorTests/MathCalculator.cs
@@ -1,9 +1,15 @@
+using System;
+using Xunit;
 namespace MathCalculatorTests 
 {
     public class MathCalculator
     {
         public int Sum(int a, int b){
             return a+b;
+        }
+
+        public int Sum(string a, string b){
+            return int.Parse(a+b);
         }
 
         public int Sub(int a, int b)
