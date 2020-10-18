886bde74879e42cd1cac55b9139f48c1cbeb4f74
Stranik <rund2504@gmail.com>
2020-10-18 12:02:35 +0300

    Bind tests with lib

--- src/RockScissorsPaperTests/WinnerCheakerTests.cs
+++ src/RockScissorsPaperTests/WinnerCheakerTests.cs
@@ -1,14 +1,15 @@
 using System;
 using Xunit;
+using RockScissorsPaperLib;
 
 namespace RockScissorsPaperTests
 {
-    public class UnitTest1
+    public class WinnerCheakerTests
     {
         [Fact]
         public void Test1()
         {
-
+            WinnerCheaker winnerCheaker = new WinnerCheaker();
         }
     }
 }
